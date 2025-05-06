1128da6f0b97cd71e4856c43dab83063b841dc4c
Hedudae <98353469+hdae@users.noreply.github.com>
2025-05-06 20:40:17 +0900

    fix: csv load issue

--- src/comlink.ts
+++ src/comlink.ts
@@ -1,7 +1,8 @@
 import { parse } from "@std/csv"
 import Fuse from "fuse.js"
+import csv_url from "./danbooru.csv?url"
 
-const fuse = fetch("/danbooru.csv")
+const fuse = fetch(csv_url)
     .then(async res => parse(await res.text()))
     .then(danbooru => new Fuse(
         danbooru.map(([w, c, p, a]) => ({ w, c, p, a: a.split(",") })),
