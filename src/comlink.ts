da7dc4faf73aa9c0690d57da7cb55bdd2f4f6259
Hedudae <98353469+hdae@users.noreply.github.com>
2025-05-06 20:33:33 +0900

    chore: fix wrong config

--- src/comlink.ts
+++ src/comlink.ts
@@ -1,11 +1,11 @@
 import { parse } from "@std/csv"
 import Fuse from "fuse.js"
 
-const fuse = await fetch("/danbooru.csv")
+const fuse = fetch("/danbooru.csv")
     .then(async res => parse(await res.text()))
     .then(danbooru => new Fuse(
         danbooru.map(([w, c, p, a]) => ({ w, c, p, a: a.split(",") })),
         { keys: ["w", "a"] }
     ))
 
-export const search = (search: string, limit: number = 20) => fuse.search(search, { limit })
+export const search = async (search: string, limit: number = 20) => (await fuse).search(search, { limit })
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import { comlink } from "vite-plugin-comlink"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), comlink()],
+  worker: {
+    plugins: () => [comlink()]
+  }
 })
